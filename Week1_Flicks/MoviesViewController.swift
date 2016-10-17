6c755bbd5dbe35cbe54975091d87914b0d1ac5c1
Mary Martinez <mlmartinez85@gmail.com>
2016-10-17 13:51:19 -0700

    Allow infinite scroll in CollectionView

--- Week1_Flicks/MoviesViewController.swift
+++ Week1_Flicks/MoviesViewController.swift
@@ -215,13 +215,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UIScrollVie
     // MARK: ScrollView delegate
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
+        let currentView = (segmentControl.selectedSegmentIndex == 0) ? tableView : collectionView
+        
         if !isMoreDataLoading {
             // Calculate the position of one screen length before the bottom of the results
-            let scrollViewContentHeight = tableView.contentSize.height
-            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            let scrollViewContentHeight = currentView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - currentView.bounds.size.height
             
             // When the user has scrolled past the threshold, start requesting
-            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && currentView.isDragging) {
                 isMoreDataLoading = true
                 
                 // ... Code to load more results ...
